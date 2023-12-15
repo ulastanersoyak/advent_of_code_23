53e40a8ae49e456474139fac799a30bad5736c3c
ulastanersoyak <ulastanersoyak@hotmail.com>
2023-12-15 18:16:57 +0300

    d14 p1 p2

--- 23/d15/main_p1.cpp
+++ 23/d15/main_p1.cpp
@@ -1,22 +1,7 @@
-#include <cstdint>
 #include <fstream>
 #include <iostream>
-#include <sstream>
 #include <string>
 #include <vector>
-
-int hash(const std::string &arg) {
-  uint64_t local = 0;
-  for (const auto ch : arg) {
-    if (ch == '\n') {
-      break;
-    }
-    local += ch;
-    local = local * 17;
-    local = local % 256;
-  }
-  return local;
-}
 
 int main(int argc, char **argv) {
   if (argc < 2) {
@@ -28,14 +13,39 @@ int main(int argc, char **argv) {
     std::cerr << "failed to open file " << argv[1];
     return -1;
   }
-  std::string segment;
-  uint64_t sum = 0;
-  while (std::getline(file, segment, ',')) {
-    std::cout << segment << '\n';
-    auto local = hash(segment);
-    sum += local;
+  std::string line;
+  std::vector<std::string> map;
+  while (std::getline(file, line)) {
+    map.push_back(line);
   }
-  std::cout << sum;
+
+  for (int i = 0; i < map.size(); i++) {
+    for (int j = 0; j < map[0].size(); j++) {
+      if (map[i][j] == 'O' && i != 0) {
+        int x_pos = i;
+        while (x_pos > 0 && map[x_pos - 1][j] == '.') {
+          map[x_pos - 1][j] = 'O';
+          map[x_pos][j] = '.';
+          x_pos--;
+        }
+      }
+    }
+  }
+
+  int rs = 0;
+  for (int i = 0; i < map.size(); i++) {
+    for (int j = 0; j < map[0].size(); j++) {
+      int x_pos = i;
+      bool does_contribute = true;
+      if (map[i][j] == 'O') {
+        rs += map.size() - i;
+      }
+    }
+  }
+  for (auto x : map) {
+    std::cout << x << '\n';
+  }
+  std::cout << rs;
   file.close();
   return 0;
 }
