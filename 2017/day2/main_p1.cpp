ad318b9a316ba7d37404ef08fc907b981d05ebef
ulastanersoyak <ulastanersoyak@hotmail.com>
2024-01-08 23:51:34 +0300

    d2 p1 p2

--- 2017/day2/main_p1.cpp
+++ 2017/day2/main_p1.cpp
@@ -1,21 +1,38 @@
-#include <vector>
+#include <algorithm>
 #include <fstream>
-#include <string>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-int main(int argc, char **argv){
-  if(argc < 2){
-     std::cerr << "usage: " << argv[0] << " <filename>\n";
+std::vector<int> parse_line(const std::string &arg) {
+  std::vector<int> rs;
+  std::istringstream iss(arg);
+  int number;
+  while (iss >> number) {
+    rs.push_back(number);
+  }
+  return rs;
+}
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    std::cerr << "usage: " << argv[0] << " <filename>\n";
     return -1;
   }
   std::ifstream file{argv[1]};
-  if(!file){
+  if (!file) {
     std::cerr << "failed to open file " << argv[1];
     return -1;
   }
   std::string line;
-  while(std::getline(file, line)){
+  int rs = 0;
+  while (std::getline(file, line)) {
+    auto vec = parse_line(line);
+    std::sort(vec.begin(), vec.end());
+    rs += (vec[vec.size()] - vec[0]);
   }
+  std::cout << rs;
   file.close();
   return 0;
 }
--- 2017/day2/main_p2.cpp
+++ 2017/day2/main_p2.cpp
@@ -1,21 +1,45 @@
-#include <vector>
+#include <algorithm>
 #include <fstream>
-#include <string>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-int main(int argc, char **argv){
-  if(argc < 2){
-     std::cerr << "usage: " << argv[0] << " <filename>\n";
+std::vector<int> parse_line(const std::string &arg) {
+  std::vector<int> rs;
+  std::istringstream iss(arg);
+  int number;
+  while (iss >> number) {
+    rs.push_back(number);
+  }
+  return rs;
+}
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    std::cerr << "usage: " << argv[0] << " <filename>\n";
     return -1;
   }
   std::ifstream file{argv[1]};
-  if(!file){
+  if (!file) {
     std::cerr << "failed to open file " << argv[1];
     return -1;
   }
   std::string line;
-  while(std::getline(file, line)){
+  int rs = 0;
+  while (std::getline(file, line)) {
+    auto vec = parse_line(line);
+    for (int i = 0; i < vec.size() - 1; i++) {
+      for (int j = i + 1; j < vec.size(); j++) {
+        if (vec[i] % vec[j] == 0) {
+          rs += vec[i] / vec[j];
+        } else if (vec[j] % vec[i] == 0) {
+          rs += vec[j] / vec[i];
+        }
+      }
+    }
   }
+  std::cout << rs;
   file.close();
   return 0;
 }
