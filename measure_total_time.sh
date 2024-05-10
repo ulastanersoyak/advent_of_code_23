8e24c794c3e7c4be527f26c516d60d2fc4db76ac
savannaretro <ulastanersoyak@hotmail.com>
2024-05-10 20:27:01 +0300

    fixed scripts

--- measure_total_time.sh
+++ measure_total_time.sh
@@ -6,10 +6,14 @@ total_time=0
 for executable in "$EXEC_DIR"/*; do
     if [ -f "$executable" ] && [ -x "$executable" ]; then
         start_time=$(date +%s.%N)
-        "$executable" > /dev/null 2>&1
+        IFS="_" read -r -a parts <<< "$executable"
+        year=${parts[0]: -4}
+        day=${parts[1]}
+        input_path="./src/$year/$day/input.txt"
+        "$executable" $input_path> /dev/null 2>&1
         end_time=$(date +%s.%N)
         execution_time=$(echo "$end_time - $start_time" | bc)
-        echo "Execution time for $(basename "$executable"): $execution_time seconds"
+        echo "execution time for $(basename "$executable"): $execution_time seconds"
         total_time=$(echo "$total_time + $execution_time" | bc)
     fi
 done
